9d776d4f8b699e4de78e9563622f2beefe8c2578
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-26 09:46:40 +0000

    update students model

--- src/models/Student.js
+++ src/models/Student.js
@@ -9,11 +9,16 @@ class Student extends Model {
             email: Sequelize.STRING,
             age: Sequelize.INTEGER,
             phone: Sequelize.STRING,
+            user_id: Sequelize.INTEGER,
         }, {
             sequelize,
         });
         
         return this;
+    }
+
+    static associate(models) {
+        this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     }
 }
 
