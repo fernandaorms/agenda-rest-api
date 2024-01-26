0e9162b83414a60eab6e359eaf13d8568b841032
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-26 22:20:49 +0000

    update home controller

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -2,16 +2,7 @@ const Contact = require('../models/Contact');
 
 class HomeController {
     async index(req, res) {
-        const newContact = await Contact.create({
-            first_name: 'Fernanda',
-            last_name: 'Ramos',
-            email: 'framos@gmail.com',
-            age: 23,
-            phone: '123456789',
-            user_id: 1,
-        });
-
-        res.json(newContact);
+        return res.json('Welcome');
     }
 }
 
