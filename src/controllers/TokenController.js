add8fd9a3e1a100c785647cb0798aa4aa8f1edef
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-26 08:19:05 +0000

    add - token routes + controller

---
+++ src/controllers/TokenController.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+class TokenController {
+    async create(req, res) {
+        try {
+            const { email = '', password = '' } = req.body;
+        
+            if(!email || !password) {
+                return res.status(401).json({
+                    errors: ['Invalid credentials']
+                });
+            }
+
+            const user = await User.findOne({ where: { email } });
+
+            if(!user) {
+                return res.status(401).json({ 
+                    errors: ['User doesn\'t exist.'] 
+                });
+            }
+
+            if(!(await user.passwordIsValid(password))) {
+                return res.status(401).json({
+                    errors: ['Invalid password']
+                });
+            }
+
+            const id = user.id;
+            const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+                 expiresIn: process.env.TOKEN_EXPIRATION,
+            });
+
+            return res.json({ token });
+        } catch(e) {
+            return res.status(400).json({ 
+                errors: e.errors.map((err) => err.message) 
+            });
+        }
+    }
+}
+
+module.exports = new TokenController();
---
+++ src/routes/tokenRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const TokenController = require('../controllers/TokenController');
+
+const router = express.Router();
+
+router.post('/', TokenController.create);
+
+module.exports = router;
