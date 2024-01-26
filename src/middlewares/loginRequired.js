4733be0634005b8f4a6464abb509208f6df44218
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-26 09:45:56 +0000

    update middleware - check if user exists before next

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     const { authorization } = req.headers;
 
     if(!authorization) {
         return res.status(401).json({
-            errors: ['Login required']
+            errors: ['Login required.']
         });
     }
 
@@ -17,13 +18,23 @@ module.exports = (req, res, next) => {
         const userData = jwt.verify(token, process.env.TOKEN_SECRET);
         const { id, email } = userData;
 
+        const user = await User.findOne({
+            where: { id, email },
+        });
+
+        if(!user) {
+            return res.status(401).json({
+                errors: ['Invalid User.']
+            });
+        }
+
         req.userId = id;
         req.userEmail = email;
 
         return next();
     } catch(e) {
         return res.status(401).json({
-            errors: ['Unexpected or invalid token']
+            errors: ['Unexpected or invalid token.']
         });
     }
 }
