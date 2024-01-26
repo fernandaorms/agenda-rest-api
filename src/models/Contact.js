380ae806d237ac53a3a3667fb1fe4fd5002b8c1f
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-26 22:21:03 +0000

    validate contact model

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -4,16 +4,55 @@ const { Model } = require('sequelize');
 class Contact extends Model {
     static init(sequelize) {
         super.init({
-            first_name: Sequelize.STRING,
-            last_name: Sequelize.STRING,
-            email: Sequelize.STRING,
-            age: Sequelize.INTEGER,
-            phone: Sequelize.STRING,
+            first_name: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                validate: {
+                    len: {
+                        args: [3, 255],
+                        msg: 'First Name field must be between 3 and 255 characters.'
+                    },
+                },
+            },
+            last_name: {
+                type: Sequelize.STRING,
+                allowNull: true,
+                validate: {
+                    len: {
+                        args: [3, 255],
+                        msg: 'Last Name field must be between 3 and 255 characters.'
+                    },
+                },
+            },
+            email: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                unique: {
+                    msg: 'Email must be unique.'
+                },
+                validate: {
+                    isEmail: {
+                        msg: 'Plase enter a valide email address.'
+                    },
+                },
+            },
+            phone: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            phone_region: {
+                type: Sequelize.VIRTUAL,
+                defaultValue: '',
+            },
+            phone_number: {
+                type: Sequelize.VIRTUAL,
+                defaultValue: '',
+            },
             user_id: Sequelize.INTEGER,
         }, {
             sequelize,
         });
-        
+
         return this;
     }
 
