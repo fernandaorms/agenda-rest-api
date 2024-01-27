1ba00ca810b6bb2c879c00e8b8607b511d8e25ef
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-27 15:20:20 +0000

    update photo controller - save on db

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,10 +1,21 @@
+const Photo = require('../models/Photo');
+
 class PhotoController {
     async create(req, res) {
         try {
-            return res.json(req.files);
-        } catch(e) {
-            console.log(e);
-            return res.status(400).json(err);
+            const user_id = req.userId;
+            let photos = [];
+
+            for (const file of req.files) {
+                const { originalname, filename } = file;
+                photos.push(await Photo.create({ originalname, filename, user_id }));
+            }
+
+            return res.json(photos);
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message)
+            });
         }
     }
 }
