25e55959235dc5de2b72594edf633bb368574013
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-27 11:54:53 +0000

    update contact routes + controller

--- src/controllers/ContactController.js
+++ src/controllers/ContactController.js
@@ -9,7 +9,6 @@ class ContactController {
         try {
             const { first_name, last_name, email, phone_region, phone_number } = req.body;
             const user_id = req.userId;
-
             let phone = null;
 
             if(phone_number) {
@@ -46,6 +45,81 @@ class ContactController {
             });
         }
     }
+
+    async show(req, res) {
+        try {
+            const contact = await Contact.findByPk(req.params.id);
+
+            if(!contact) {
+                return res.status(400).json({ 
+                    errors: ['Contact doesn\'t exist.'] 
+                });
+            }
+
+            return res.json(contact);
+        } catch(e) {
+            return res.json(null);
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const contact = await Contact.findByPk(req.params.id);
+
+            if(!contact) {
+                return res.status(400).json({ 
+                    errors: ['Contact doesn\'t exist.'] 
+                });
+            }
+
+            const { first_name, last_name, email, phone_region, phone_number } = req.body;
+            let phone = null;
+
+            if(phone_number) {
+                const formattedNumber = phoneValidation(phone_number, phone_region ? phone_region : 'BR');
+                
+                if(!formattedNumber) {
+                    return res.status(400).json({
+                        errors: ['Invalid phone number for the provided region.']
+                    });
+                }
+
+                phone = formattedNumber;
+            }
+
+
+            const updatedContact = await contact.update({ first_name, last_name, email, phone });
+
+            return res.json(updatedContact);
+        } catch(e) {
+            return res.status(400).json({ 
+                errors: e.errors.map((err) => err.message) 
+            });
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            const contact = await Contact.findByPk(req.params.id);
+
+            if(!contact) {
+                return res.status(400).json({
+                    errors: ['Contact doesn\'t exist.']
+                });
+            }
+
+            await contact.destroy();
+
+            return res.json({ delete: true });
+        } catch(e) {
+            return res.status(400).json({ 
+                errors: e.errors.map((err) => err.message) 
+            });
+        }
+    }
+
+
+
 }
 
 module.exports = new ContactController();
--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const contactController = require('../controllers/ContactController');
 const loginRequired = require('../middlewares/loginRequired');
 
@@ -6,6 +7,9 @@ const router = express.Router();
 
 router.post('/', loginRequired, contactController.create);
 router.get('/', loginRequired, contactController.index);
+router.get('/:id', loginRequired, contactController.show);
+router.put('/:id', loginRequired, contactController.update);
+router.delete('/:id', loginRequired, contactController.delete);
 
 
 module.exports = router;
