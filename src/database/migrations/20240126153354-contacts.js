919dd4175438e405962114cf2118383cda9558fc
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-27 12:04:19 +0000

    update - migration create table contacts

--- src/database/migrations/20240126153354-contacts.js
+++ src/database/migrations/20240126153354-contacts.js
@@ -52,7 +52,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.removeColumn('contacts', 'user_id');
         await queryInterface.dropTable('contacts');
     }
 };
