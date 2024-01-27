92543266526a806e2b608ba11896ca1f51c01e63
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-27 17:17:23 +0000

    update show route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,8 +6,7 @@ const loginRequired = require('../middlewares/loginRequired');
 const router = express.Router();
 
 router.get('/', userController.index);
-// router.get('/:id', userController.show);
-
+// router.get('/', loginRequired, userController.show);
 router.post('/', userController.create);
 router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
